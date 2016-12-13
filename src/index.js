db56b5873c52c57496268dbbf54a24fa82dd4b0d
frontmaker <Djeka345@gmail.com>
2016-12-13 16:44:31 +0300

    Create structure of the game

--- src/index.js
+++ src/index.js
@@ -2,4 +2,91 @@ import './style/main.scss';
 
 
 
-console.log(3);
+class MoleGame  {
+	constructor() {
+		
+		this.holes = document.querySelectorAll('.hole');
+		this.scoreBoard = document.querySelector('.score');
+		this.moles = document.querySelectorAll('.mole');
+		this.lastHole = '';
+		this.timeUp = false;
+	}
+
+
+	randomTime(min,max) {
+		return Math.round(Math.random() * (max - min) + min);
+	}
+
+	randomHole(holes) {
+		const idx = Math.floor(Math.random() * holes.length);
+
+		const hole = this.holes[idx];
+
+		if (hole === this.lastHole) {
+			return this.randomHole(holes);
+		}
+
+		this.lastHole = hole;
+
+		return hole;
+	} 
+
+	handleClick(hole) {
+
+		console.log(hole);
+
+		hole.onclick = () => {
+			this.scoreBoard.innerHTML++;
+			hole.classList.remove('up');
+		}
+
+
+		// hole.addEventListener('click', () => {
+
+		// 	console.log(this.scoreBoard.innerHTML);
+
+		// });
+
+	}
+
+	finish(time) {
+		setTimeout(() => {
+			this.timeUp = true;
+		}, time)
+	}
+
+
+	peep() {
+		const time = this.randomTime(200, 1000);
+		const hole = this.randomHole(this.holes);
+
+		hole.classList.add('up');
+
+		this.handleClick(hole);
+
+		setTimeout(() => {
+
+			hole.classList.remove('up');
+			
+			if (!this.timeUp) this.peep();
+
+			this.finish(2000);
+
+		}, time);
+	}
+
+
+
+
+
+	start() {
+
+		this.randomHole(this.holes);
+		this.peep();
+	}
+}
+
+
+const game = new MoleGame();
+
+game.start();
