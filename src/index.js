7c1b2136a777f786e50097c56a8210102e6b7ece
frontmaker <Djeka345@gmail.com>
2016-12-13 17:28:54 +0300

    Handling click event on mole

--- src/index.js
+++ src/index.js
@@ -2,88 +2,94 @@ import './style/main.scss';
 
 
 
-class MoleGame  {
-	constructor() {
-		
-		this.holes = document.querySelectorAll('.hole');
-		this.scoreBoard = document.querySelector('.score');
-		this.moles = document.querySelectorAll('.mole');
-		this.lastHole = '';
-		this.timeUp = false;
-	}
+class MoleGame {
+    constructor() {
 
 
-	randomTime(min,max) {
-		return Math.round(Math.random() * (max - min) + min);
-	}
-
-	randomHole(holes) {
-		const idx = Math.floor(Math.random() * holes.length);
-
-		const hole = this.holes[idx];
-
-		if (hole === this.lastHole) {
-			return this.randomHole(holes);
-		}
-
-		this.lastHole = hole;
-
-		return hole;
-	} 
-
-	handleClick(hole) {
-
-		console.log(hole);
-
-		hole.onclick = () => {
-			this.scoreBoard.innerHTML++;
-			hole.classList.remove('up');
-		}
+        this.holes = document.querySelectorAll('.hole');
+        this.scoreBoard = document.querySelector('.score');
+        this.moles = document.querySelectorAll('.mole');
+        this.lastHole = '';
+        this.score = 0;
+        this.timeUp = false;
 
 
-		// hole.addEventListener('click', () => {
-
-		// 	console.log(this.scoreBoard.innerHTML);
-
-		// });
-
-	}
-
-	finish(time) {
-		setTimeout(() => {
-			this.timeUp = true;
-		}, time)
-	}
+    }
 
 
-	peep() {
-		const time = this.randomTime(200, 1000);
-		const hole = this.randomHole(this.holes);
+    randomTime(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
 
-		hole.classList.add('up');
+    randomHole(holes) {
+        const idx = Math.floor(Math.random() * holes.length);
 
-		this.handleClick(hole);
+        const hole = this.holes[idx];
 
-		setTimeout(() => {
+        if (hole === this.lastHole) {
+            return this.randomHole(holes);
+        }
 
-			hole.classList.remove('up');
-			
-			if (!this.timeUp) this.peep();
+        this.lastHole = hole;
 
-			this.finish(2000);
+        return hole;
+    }
 
-		}, time);
-	}
+    finish(time) {
+        setTimeout(() => {
+            this.timeUp = true;
+        }, time)
+    }
+
+
+    handleBonk() {
+
+
+    	this.moles.forEach((mole) => mole.addEventListener('click', function(e) {
+
+    		if (!e.isTrusted) return;
+
+    		updateScore();
+
+    		this.classList.remove('up');
+
+    	}));
+
+
+    	const updateScore = () => {
+    		this.scoreBoard.textContent++;
+    	}
+
+    }
+
+
+    peep() {
+        const time = this.randomTime(200, 1000);
+        const hole = this.randomHole(this.holes);
+
+        hole.classList.add('up');
+
+        setTimeout(() => {
+
+            hole.classList.remove('up');
+
+            if (!this.timeUp) this.peep();
+
+            this.finish(10000);
+
+        }, time);
+    }
 
 
 
 
 
-	start() {
+    start() {
 
-		this.randomHole(this.holes);
-		this.peep();
-	}
+        this.randomHole(this.holes);
+        this.handleBonk();
+        this.peep();
+    }
 }
 
 
